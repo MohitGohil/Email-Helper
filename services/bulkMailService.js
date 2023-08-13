05bace196e60c8251fd2a309263897996255f25c
Mohit <mohitgohil571@gmail.com>
2023-08-13 22:51:20 +0530

    Single/Multiple Mail service Created

---
+++ services/bulkMailService.js
@@ -0,0 +1,59 @@
+require("dotenv").config();
+const nodemailer = require("nodemailer");
+
+// Send emails to multiple users
+class SendBulkMail {
+  constructor(userInput) {
+    const { email, subject, content, cc, attachments } = userInput;
+    if (!email || !subject || !content) {
+      throw new Error("Email, subject and content are required.");
+    }
+    this.emails = Array.isArray(email) ? email : [email];
+    this.cc = cc ? (isArray(cc) ? cc : [cc]) : [];
+    this.subject = subject;
+    this.content = content;
+    this.attachments = attachments || [];
+  }
+  // Connecting with gmail with credentials
+  async sendMail() {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.GMAIL_USERNAME,
+        pass: process.env.GMAIL_PASSWORD,
+      },
+    });
+    // Create return object template
+    const result = { success: [], invalidEmail: [] };
+    // For loop for sending same email content to multiple users
+    for (let i = 0; i < this.emails.length; i++) {
+      const email = this.emails[i];
+      if (this.isValidEmail(email)) {
+        const mailOptions = {
+          from: `Test Mail - Nodejs App <${process.env.GMAIL_USERNAME}>`,
+          to: email,
+          cc: this.cc,
+          subject: this.subject,
+          html: this.content,
+          attachments: this.attachments,
+        };
+        try {
+          const sentEmail = await transporter.sendMail(mailOptions);
+          result.success.push({ email, messageId: sentEmail.messageId });
+        } catch (error) {
+          console.error(`Bulk Email Error - ${error}`);
+        }
+      } else {
+        result.invalidEmail.push(email);
+      }
+    }
+    return result;
+  }
+  // Email format check
+  isValidEmail(email) {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email);
+  }
+}
+
+module.exports = SendBulkMail;
---
+++ services/mailService.js
@@ -0,0 +1,74 @@
+const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
+require("dotenv").config();
+
+class Mailer {
+  constructor(userInput) {
+    const { email, subject, content, cc, attachments } = userInput;
+    if (!email || !subject || !content) {
+      throw new Error("Email, subject and content are required.");
+    }
+    // User input fields
+    this.emails = Array.isArray(email) ? email : [email];
+    this.cc = cc ? (isArray(cc) ? cc : [cc]) : [];
+    this.subject = subject;
+    this.content = content;
+    this.attachments = attachments || [];
+    // Fields required to send mail
+    this.clientId = process.env.CLIENT_ID;
+    this.clientSecret = process.env.CLIENT_SECRET;
+    this.redirectUri = process.env.REDIRECT_URI || "https://developers.google.com/oauthplayground";
+    this.refreshToken = process.env.REFRESH_TOKEN;
+    this.gmailAppPass = process.env.GMAIL_APP_PASS;
+    // Generate refresh token
+    this.oAuth2Client = new google.auth.OAuth2(this.clientId, this.clientSecret, this.redirectUri);
+    this.oAuth2Client.setCredentials({ refresh_token: this.refreshToken });
+    // OAuth2 Config
+    this.transport = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: `${process.env.GMAIL_USERNAME}`,
+        clientId: this.clientId,
+        clientSecret: this.clientSecret,
+        refreshToken: this.refreshToken,
+        accessToken: this.oAuth2Client.getAccessToken(),
+      },
+    });
+  }
+
+  async sendMail() {
+    // Remove invalid emails from the array
+    const receiversEmail = this.emails;
+    for (let i = 0; i < receiversEmail.length; i++) {
+      const email = receiversEmail[i];
+      if (!this.isValidEmail(email)) {
+        receiversEmail.pop(email);
+      }
+    }
+    // Generate email template
+    const mailOptions = {
+      from: `Test Mail - Nodejs App <${process.env.GMAIL_USERNAME}>`,
+      to: receiversEmail.join(","),
+      cc: this.cc.join(","),
+      subject: this.subject,
+      html: this.content,
+      attachments: this.attachments,
+    };
+    // sending email to users
+    try {
+      const sentEmailResult = await this.transport.sendMail(mailOptions);
+      return sentEmailResult;
+    } catch (err) {
+      console.log("Error - Mailer.sendMail", err);
+      throw new Error(err);
+    }
+  }
+  // check email format
+  isValidEmail(email) {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email);
+  }
+}
+
+module.exports = Mailer;
